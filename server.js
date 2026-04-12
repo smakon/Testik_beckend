4cd51d83bb9ac4edc269529e9f7eaeee6d9f63a9
smakon <25469@inbox.ru>
2026-04-12 21:39:35 +0300

    Update 3

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 require('dotenv').config()
 const app = require('./src/app')
 
-const PORT = 8080
 const HOST = '0.0.0.0'
 
 app.listen(PORT, HOST, () => {
-	console.log(`Server listening on http://${HOST}:${PORT}`)
+	console.log(`Server listening on http://${HOST}`)
 })
