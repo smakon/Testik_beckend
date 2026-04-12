58487a64d289847988241102c6ee47e53488e40b
smakon <25469@inbox.ru>
2026-04-12 23:16:41 +0300

    Add site

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const cors = require('cors')
 const helmet = require('helmet')
 const morgan = require('morgan')
 const cookieParser = require('cookie-parser')
+const path = require('path')
 
 const userRoutes = require('./routes/userRouters')
 const testsRouter = require('./routes/testRouters')
@@ -19,14 +20,15 @@ app.use(morgan('dev'))
 app.use(express.json())
 app.use(cors())
 
-
+app.use(express.static(path.join(__dirname, 'dist')))
 // Routes
 app.use('/api/users', userRoutes)
 app.use('/api/tests', testsRouter)
 app.use('/api/questions', questionsRouter)
 app.use('/api/results', resultsRouter)
-app.use('/', ((req, res) => {res.send('Hello from the backend!')}))
-
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, 'dist', 'index.html'))
+})
 // Обработка 404
 app.use((req, res) => {
   res.status(404).json({ error: req });
